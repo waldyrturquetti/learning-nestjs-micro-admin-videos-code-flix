21c1c8f17441c99bf7c2588da5a5331f00a1b81e
waldyr <waldyr.turquetti@escala-app.com>
2023-01-08 14:54:00 -0300

    feat: created the constructor and unit test.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,32 @@
 import { Category } from "./category";
 
 describe("Category Tests", () => {
-    test('constructor of category', () => {
-        const category = new Category('Movie');
-        expect(category.name).toBe('Movie');
-    })
+  test("constructor of category", () => {
+    // Triple AAA - Arrange Act Assert
+
+    const created_at = new Date();
+    // Arrange
+    const props = {
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: created_at,
+    };
+
+    // Act
+    const category = new Category(props);
+
+    // Assert
+    // expect(category.name).toBe("Movie");
+    // expect(category.description).toBe("some description");
+    // expect(category.is_active).toBeTruthy;
+    // expect(category.created_at).toBe(created_at);
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: created_at,
+    });
+  });
 });
